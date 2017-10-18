60ad24191b974186b0125498398f680e4607dd87
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-10-18 13:26:21 +1300

    removed todo statement from last commit

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -447,8 +447,7 @@ public class PlayerMotor : MonoBehaviour
      *  Other pieces of scenery that rotate when gravity shifts
      *  paths that can only be accessed when gravity is in a particular direction?
      */
-
-    // TODO: There is a bug somewhere relating to rotation - sometimes camera horizontal movement is prevented while airborne - so far only occured with irregular grav direction
+     
     //perform rotation based on rotation variable
     private void PerformRotation()
     {
