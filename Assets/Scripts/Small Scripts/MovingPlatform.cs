560e9dfcbd76d46bdc6ddb434566464c2edf0846
Chris Read <ChrisR@intergen.org.nz>
2017-12-07 10:20:39 +1300

    tested alternative relative movement code within MovingPlatform script. worked slightly better than playermovement code, but still exhibited the bug when turning the camera

--- Assets/Scripts/Small Scripts/MovingPlatform.cs
+++ Assets/Scripts/Small Scripts/MovingPlatform.cs
@@ -13,6 +13,10 @@ public class MovingPlatform : MonoBehaviour
 
     private Rigidbody rb;
 
+    //private Vector3 lastMovementVector = Vector3.zero;
+
+    //private List<Rigidbody> attachedObjects = new List<Rigidbody>();
+
     void OnEnable()
     {
         if (startPos == endPos)
@@ -26,14 +30,70 @@ public class MovingPlatform : MonoBehaviour
 
 	// Update is called once per frame
 	void Update () {
-	    rb.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+        //Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+	    //lastMovementVector = newPos - rb.position;
+	    //rb.position = newPos;
 
-	    if (transform.position == targetPos)
+        rb.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+
+        if (transform.position == targetPos)
 	    {
 	        if (targetPos == startPos)
 	            targetPos = endPos;
 	        else
 	            targetPos = startPos;
 	    }
+
+        // move attached objects
+	    //foreach (Rigidbody obj in attachedObjects)
+	    //{
+	    //    obj.position += lastMovementVector;
+	    //}
 	}
+
+    //void OnCollisionStay(Collision col)
+    //{
+    //    bool isOnTop = false;
+
+    //    // check if has a RigidBody
+    //    Rigidbody colRb = col.gameObject.GetComponent<Rigidbody>();
+    //    if (colRb == null)
+    //        return;
+
+    //    foreach (ContactPoint item in col.contacts)
+    //    {
+    //        isOnTop = item.point.y > transform.position.y;
+    //        if (isOnTop)
+    //            break;
+    //    }
+    //    if (isOnTop)
+    //    {
+    //        bool unique = true;
+    //        foreach (Rigidbody attached in attachedObjects)
+    //        {
+    //            unique = attached != colRb;
+
+    //            if (!unique)
+    //                break;
+    //        }
+    //        if (unique)
+    //        {
+    //            attachedObjects.Add(colRb);
+    //        }
+    //    }
+    //}
+
+    //void OnCollisionExit(Collision col)
+    //{
+    //    Rigidbody colRb = col.gameObject.GetComponent<Rigidbody>();
+    //    if (colRb == null)
+    //        return;
+
+    //    if (attachedObjects.Contains(colRb))
+    //    {
+    //        // add velocity/force in direction of last movement
+
+    //        attachedObjects.Remove(colRb);
+    //    }
+    //}
 }
