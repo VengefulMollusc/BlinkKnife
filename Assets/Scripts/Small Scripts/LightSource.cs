bb5f53c7d226ba9d1b72628504d40a532d9b898e
Chris Read <ChrisR@intergen.org.nz>
2018-04-30 10:44:50 +1200

    began construction of light source script - switching to new branch

---
+++ Assets/Scripts/Small Scripts/LightSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour
+{
+    public static float updateFrequency = 0.1f;
+
+    private Light light;
+
+    void OnEnable()
+    {
+        light = GetComponent<Light>();
+
+        InvokeRepeating("LightSensorCheck", 0f, updateFrequency);
+    }
+
+    private void LightSensorCheck()
+    {
+        
+    }
+}
