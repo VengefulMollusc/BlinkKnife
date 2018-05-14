88c610039ac4ddf7a0000c6a389cf23bc36c6589
Chris Read <ChrisR@intergen.org.nz>
2018-05-14 16:21:42 +1200

    tidied PlayerController and discovered bug with player 'sticking' to some edges when trying to walk off them

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    
-	private static float speed = 5.0f;
+
+    private static float speed = 5.0f;
     private static float sprintModifier = 1.75f;
     private static float sprintThreshold = 0.45f;
 
@@ -12,130 +13,108 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     [Range(0f, 1f)]
     private float backMoveMax = 0.9f;
-	[SerializeField]
-	private float lookSensitivity = 3.0f;
-	[SerializeField]
-	private float jumpStrength = 100f;
+    [SerializeField]
+    private float lookSensitivity = 3.0f;
+    [SerializeField]
+    private float jumpStrength = 100f;
 
-	// controller axis settings
-	[Header("Control Settings")]
-	[SerializeField]
-	private string xMovAxis = "Horizontal";
-	[SerializeField]
-	private string zMovAxis = "Vertical";
-	[SerializeField]
-	private string xLookAxis = "LookX";
-	[SerializeField]
-	private string yLookAxis = "LookY";
-	[SerializeField]
-	private string jumpButton = "Jump";
-	[SerializeField]
-	private string sprintButton = "Sprint";
+    // controller axis settings
+    [Header("Control Settings")]
+    [SerializeField]
+    private string xMovAxis = "Horizontal";
+    [SerializeField]
+    private string zMovAxis = "Vertical";
+    [SerializeField]
+    private string xLookAxis = "LookX";
+    [SerializeField]
+    private string yLookAxis = "LookY";
+    [SerializeField]
+    private string jumpButton = "Jump";
+    [SerializeField]
+    private string sprintButton = "Sprint";
     [SerializeField]
     private string crouchButton = "Crouch";
 
     private PlayerMotor motor;
 
-	void Start (){
-		motor = GetComponent<PlayerMotor> ();
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	}
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
-	void Update (){
+    void Update()
+    {
 
-		// calculate movement as 3d vector
-		float xMov = Input.GetAxisRaw (xMovAxis); // USE GetAxisRaw for unsmoothed input
-		float zMov = Input.GetAxisRaw (zMovAxis); // GetAxis for smoothed
+        // Gets vertical and horizontal input vectors
+        float xMov = Input.GetAxisRaw(xMovAxis);
+        float zMov = Input.GetAxisRaw(zMovAxis);
 
         // apply backwards movement limit
         zMov = Mathf.Clamp(zMov, -backMoveMax, 1.0f);
 
-		// multiply local directions by current movement values
-		Vector3 movHorizontal = transform.right * xMov;
-		Vector3 movVertical = transform.forward * zMov;
+        // multiply local directions by current movement values
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
 
-		// final movement vector
-		Vector3 velocity = movHorizontal + movVertical;
-		if (velocity.magnitude > 1.0f)
-			velocity.Normalize ();
-		velocity = velocity * speed;
+        // final movement vector
+        Vector3 velocity = movHorizontal + movVertical;
+        if (velocity.magnitude > 1.0f)
+            velocity.Normalize();
+        velocity = velocity * speed;
 
-        // apply sprint speed
-  //      bool sprinting = false;
-  //      if (zMov > sprintThreshold && Input.GetButton(sprintButton))
-  //      {
-  //          velocity = velocity * sprintModifier;
-  //          sprinting = true;
-		//}
+        // get sprinting boolean
+        bool sprinting = (zMov > sprintThreshold && Input.GetButton(sprintButton));
 
-	    bool sprinting = (zMov > sprintThreshold && Input.GetButton(sprintButton));
+        // Pass movement variables to PlayerMotor
+        motor.Move(velocity, sprinting);
 
-        // apply movement
-        motor.Move (velocity, sprinting);
+        // calculate rotation as 3d vector: for turning on y axis
+        float yRot = Input.GetAxisRaw(xLookAxis) * Time.deltaTime;
+        Vector3 rotation = new Vector3(0.0f, yRot, 0.0f) * lookSensitivity;
 
-		// calculate rotation as 3d vector: for turning on y axis
-		float yRot = Input.GetAxisRaw (xLookAxis) * Time.deltaTime;
+        // calculate camera rotation as angle: for turning on x axis
+        float xRot = Input.GetAxisRaw(yLookAxis) * Time.deltaTime;
+        float cameraRotationX = xRot * lookSensitivity;
 
-		Vector3 rotation = new Vector3 (0.0f, yRot, 0.0f) * lookSensitivity;
+        // Pass rotation variables to PlayerMotor
+        motor.LookRotation(rotation, cameraRotationX);
 
-		// apply rotation
-		//motor.Rotate (rotation);
+        // Check for Jump
+        if (Input.GetButtonDown(jumpButton))
+        {
+            // jump code here, pass to motor
+            motor.Jump(jumpStrength);
+        }
+        else if (Input.GetButton(jumpButton))
+        {
+            // jump button being held
+            motor.JumpHold();
+        }
 
-		// calculate camera rotation as 3d vector: for turning on x axis
-		float xRot = Input.GetAxisRaw (yLookAxis) * Time.deltaTime;
-
-		float cameraRotationX = xRot * lookSensitivity;
-
-		// apply rotation
-		//motor.RotateCamera (cameraRotationX);
-
-	    motor.LookRotation(rotation, cameraRotationX);
-
-		// jump code
-		if (Input.GetButtonDown (jumpButton)) {
-			// jump code here, pass to motor
-			motor.Jump(jumpStrength);
-		}
-	    if (Input.GetButton(jumpButton))
-	    {
-	        // jump button being held
-	        motor.JumpHold();
-	    }
-
-        // crouch code
+        // Check for Crouch
         if (Input.GetButtonDown(crouchButton))
         {
             motor.SetCrouching(true);
-        } else if (Input.GetButtonUp(crouchButton))
+        }
+        else if (Input.GetButtonUp(crouchButton))
         {
             motor.SetCrouching(false);
         }
-
-
-        // Test controls for health/energy
-        //if (Input.GetKeyDown(KeyCode.K))
-        //{
-        //    float mod = Random.Range(-2f, -30f);
-        //    motor.ModifyHealth(mod);
-        //    motor.ModifyEnergy(mod);
-        //    Debug.Log("Health/Energy decreased: " + mod);
-        //}
-        //if (Input.GetKeyDown(KeyCode.L))
-        //{
-        //    float mod = Random.Range(2f, 30f);
-        //    motor.ModifyHealth(mod);
-        //    motor.ModifyEnergy(mod);
-        //    Debug.Log("Health/Energy increased: " + mod);
-        //}
-
     }
 
+    /*
+     * Returns the base speed value
+     */
     public static float Speed()
     {
         return speed;
     }
 
+    /*
+     * returns the sprint modifier
+     */
     public static float SprintModifier()
     {
         return sprintModifier;
