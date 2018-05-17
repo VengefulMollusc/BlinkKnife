49d8ebb38128a1da0b0416c7e265d198c4b80cae
Chris Read <ChrisR@intergen.org.nz>
2018-05-17 12:04:54 +1200

    tidying boostring

--- Assets/Scripts/BoostRing.cs
+++ Assets/Scripts/BoostRing.cs
@@ -6,6 +6,8 @@ using UnityEngine;
  * Controls boost ring behaviour.
  * Needs to be attached to gameobject with boost ring trigger collider -
  * assumes is child of boost ring root object
+ * 
+ * TODO: rename/change to light theme. eg: Lens?
  */
 public class BoostRing : MonoBehaviour
 {
@@ -20,17 +22,10 @@ public class BoostRing : MonoBehaviour
     private Vector3 previousTarget;
     private Vector3 nextTarget;
 
-    //private Transform boostRingRoot;
-
     public const string BoostNotification = "BoostRing.BoostNotification";
 
-    private void OnEnable()
+    private void Start()
     {
-        //boostRingRoot = transform.parent;
-
-        //previousTarget = boostRingRoot.position + (-boostRingRoot.up * 10f);
-        //nextTarget = boostRingRoot.position + (boostRingRoot.up * 10f);
-
         CheckRingChain();
     }
 
@@ -39,16 +34,18 @@ public class BoostRing : MonoBehaviour
      */
     private void CheckRingChain()
     {
+        // check for next ring
         if (nextRing != null)
             nextTarget = nextRing.position;
 
+        // check for previous ring
         if (previousRing != null)
             previousTarget = previousRing.position;
     }
 
     private Vector3 GetBoostVector(Vector3 toBoost, Vector3 target)
     {
-        return (target - toBoost);
+        return (target - toBoost).normalized;
     }
 
     void OnTriggerEnter(Collider col) // could be OnTriggerStay/Enter
@@ -71,12 +68,9 @@ public class BoostRing : MonoBehaviour
             {
                 boostDirection = GetBoostVector(col.transform.position, nextTarget);
                 // record distance/time here to next ring if needed for gravity cancelling duration
-                boostDirection.Normalize();
             }
             else
-            {
                 boostDirection = transform.up;
-            }
         }
         else
         {
@@ -84,18 +78,13 @@ public class BoostRing : MonoBehaviour
             {
                 boostDirection = GetBoostVector(col.transform.position, previousTarget);
                 // record distance/time here to next ring if needed for gravity cancelling duration
-                boostDirection.Normalize();
             }
             else
-            {
                 boostDirection = -transform.up;
-            }
         }
-        
-        //Debug.Log("Boosting: " + col.gameObject.name + " - " + boostDirection);
 
         Info<GameObject, Vector3> info = new Info<GameObject, Vector3>(col.gameObject, boostDirection * magnitude);
-        // Individual objects can handle different logic
+        // notification so individual objects can handle different logic
         this.PostNotification(BoostNotification, info);
     }
 }
