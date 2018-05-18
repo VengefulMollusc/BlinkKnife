d7887f169326adf80fcffd4c29e89f274904a584
Chris Read <ChrisR@intergen.org.nz>
2018-05-18 16:09:11 +1200

    expanded KnifeInteractionTrigger to allow triggering on warp

--- Assets/Scripts/Interactables/KnifeInteractionTrigger.cs
+++ Assets/Scripts/Interactables/KnifeInteractionTrigger.cs
@@ -10,6 +10,9 @@ public class KnifeInteractionTrigger : InteractionTrigger
      * Platform position is toggled when knife hits it etc.
      */
 
+    // only activate when player warps to this object
+    [SerializeField] private bool activateOnWarp;
+
     // only active while knife is attached. deactivate when knife is removed
     [SerializeField] private bool activeWhileKnifeAttached;
 
@@ -18,11 +21,13 @@ public class KnifeInteractionTrigger : InteractionTrigger
     void OnEnable()
     {
         this.AddObserver(OnKnifeReturnTransition, KnifeController.ReturnKnifeTransitionNotification);
+        this.AddObserver(OnWarpEndNotification, TransitionCameraController.WarpEndNotification);
     }
 
     void OnDisable()
     {
         this.RemoveObserver(OnKnifeReturnTransition, KnifeController.ReturnKnifeTransitionNotification);
+        this.RemoveObserver(OnWarpEndNotification, TransitionCameraController.WarpEndNotification);
     }
 
     // De-attaches knife on return and Triggers objects again if active only while knife attached
@@ -37,12 +42,32 @@ public class KnifeInteractionTrigger : InteractionTrigger
         }
     }
 
+    // calls knife return logic on warp end. Also toggles triggers if activateOnWarp
+    void OnWarpEndNotification(object sender, object args)
+    {
+        if (!knifeAttached)
+            return;
+
+        if (activateOnWarp)
+        {
+            knifeAttached = false;
+            ToggleTriggers();
+        }
+        else
+        {
+            OnKnifeReturnTransition(null, null);
+        }
+    }
+
     /*
      * Attaches knife and triggers objects
      */
     public void AttachKnife()
     {
         knifeAttached = true;
+
+        if (activateOnWarp)
+            return;
 
         if (activeWhileKnifeAttached)
             ActivateTriggers(true);
