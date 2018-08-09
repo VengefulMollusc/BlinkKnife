cf87d53ca6de779d17ad730ef3ee8b3c75c428aa
Chris Read <ChrisR@intergen.org.nz>
2018-08-10 10:31:05 +1200

    removed commented code

--- Assets/Scripts/Vehicles/HoverBikeController.cs
+++ Assets/Scripts/Vehicles/HoverBikeController.cs
@@ -6,16 +6,9 @@ public class HoverBikeController : MonoBehaviour
 {
     public InputSettings inputSettings;
     public HoverMotor motor;
-    //public Transform cameraPositionTransform;
-
-    private Transform cameraTransform;
-    //private const float cameraRotLimit = 90f;
-    //private float currentCamRotX;
 
     void Start()
     {
-        //cameraTransform = transform.GetChild(0);
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -36,32 +29,5 @@ public class HoverBikeController : MonoBehaviour
         float yCam = Input.GetAxisRaw(inputSettings.yLookAxis);
 
         motor.Turn(xCam, yCam);
-
-        //MoveCamera(new Vector2(xCam, yCam) * inputSettings.lookSensitivity);
     }
-
-    //void LateUpdate()
-    //{
-    //    transform.position = cameraPositionTransform.position;
-    //}
-
-    //void MoveCamera(Vector2 input)
-    //{
-    //    // calculate rotation as 3d vector: for turning on y axis
-    //    float yRot = input.x * Time.deltaTime;
-    //    Vector3 rotation = new Vector3(0.0f, yRot, 0.0f);
-
-    //    // calculate camera rotation as angle: for turning on x axis
-    //    float xRot = input.y * Time.deltaTime;
-
-    //    // Rotate player for horizontal camera movement
-    //    transform.rotation *= Quaternion.Euler(rotation);
-
-    //    // rotation calculation - clamps to limit values
-    //    currentCamRotX -= xRot;
-    //    currentCamRotX = Mathf.Clamp(currentCamRotX, -cameraRotLimit, cameraRotLimit);
-
-    //    // apply rotation to transform of camera
-    //    cameraTransform.localEulerAngles = new Vector3(currentCamRotX, 0f, 0f);
-    //}
 }
