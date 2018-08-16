cc222425933c309539a33133d4a4cea92641a686
Chris Read <ChrisR@intergen.org.nz>
2018-08-16 14:10:07 +1200

    added knifeWaveCollisionHandler. currently just returns knife on collision

---
+++ Assets/Scripts/ShaderScripts/WaveCollisionHandlers/KnifeWaveCollisionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeWaveCollisionHandler : WaveCollisionHandler
+{
+    private KnifeController knifeController;
+
+    // Use this for initialization
+    void Start()
+    {
+        knifeController = GetComponent<KnifeController>();
+    }
+
+    public override void CollideWithWave(WavePositionInfo waveInfo)
+    {
+        if (waveInfo.position.y > transform.position.y)
+        {
+            knifeController.ReturnKnifeTransition();
+        }
+    }
+}
